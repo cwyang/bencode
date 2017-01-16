7fb81cc5b03d80fa310f1d5afcc1b8c4fbbc1206
Chul-Woong Yang <cwyang@gmail.com>
2017-01-16 10:02:10 +0900

    untabify

--- list.h
+++ list.h
@@ -15,15 +15,15 @@
 #define SIMPLE_LIST_H
 
 typedef struct list_head {
-	struct list_head *next, *prev;
+    struct list_head *next, *prev;
 } list_t;
 
 #define LIST_HEAD_INIT(name) { &(name), &(name) }
 #define LIST_HEAD(name) list_t name = LIST_HEAD_INIT(name)
 
 static inline list_t* init_list_head(list_t *head) {
-	head->next = head->prev = head;
-	return head;
+    head->next = head->prev = head;
+    return head;
 }
 
 static inline void list_add(list_t *new, list_t *head) {
@@ -46,21 +46,21 @@ static inline void list_del(list_t *entry) {
 }
 
 static inline int list_empty(list_t *head) {
-	return head->next == head;
+    return head->next == head;
 }
 
 #ifdef container_of
 #define list_entry container_of
 #else
 #define list_entry(ptr, type, member)                               \
-	((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
+    ((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
 #endif
 
-#define list_for_each(POS, HEAD)                                    \
-	for ((POS) = (HEAD)->next; (POS) != (HEAD); (POS) = (POS)->next)
-        	
+#define list_for_each(POS, HEAD)                                        \
+    for ((POS) = (HEAD)->next; (POS) != (HEAD); (POS) = (POS)->next)
+            
 #define list_for_each_safe(POS, TEMP, HEAD)                    \
-	for ((POS) = (HEAD)->next, (TEMP) = (POS)->next;           \
+    for ((POS) = (HEAD)->next, (TEMP) = (POS)->next;           \
          (POS) != (HEAD);                                      \
          (POS) = (TEMP), (TEMP) = (POS)->next)
 
